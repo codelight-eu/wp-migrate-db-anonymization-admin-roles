2387c417a78af887d83a1975cdf466abd156c277
Indrek Kõnnussaar <indrek@codelight.eu>
2019-08-28 19:37:23 +0300

    Initial commit

---
+++ wp-migrate-db-anonymization-admin-roles.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: WP Migrate DB Anonymization Admin Roles
+Plugin URI: https://codelight.eu
+Description: Prevents anonymizing Administrator, Editor and Shop Manager roles
+Author: Codelight
+Version: 1.0
+Author URI: https://codelight.eu
+*/
+
+add_filter('wpmdb_anonymization_user_whitelisted', function($whitelisted, $user) {
+
+	if (in_array('administrator', (array) $user->roles) || 
+		in_array('editor', (array) $user->roles) || 
+		in_array('shop-manager', (array) $user->roles ||
+		user_can($user, 'manage_options')	
+	)
+	) {
+		return true;
+	}
+
+	return $whitelisted;
+
+}, 10, 2);
